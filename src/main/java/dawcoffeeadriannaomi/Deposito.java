e2b99e8d47ec8ee41e34f3d8e694a955ce29b502
Adrianpg <adrianperezgomezsmr@gmail.com>
2020-01-08 23:05:47 +0100

    Añade atributos, constructores,to string, getters y setters y metodo privado

--- src/main/java/dawcoffeeadriannaomi/Deposito.java
+++ src/main/java/dawcoffeeadriannaomi/Deposito.java
@@ -10,5 +10,74 @@ package dawcoffeeadriannaomi;
  * @author adrip
  */
 public class Deposito {
-  
+  //Añadimos atributos de la clase Depósito
+    private String contenido;
+    private double cantidadMaxima;
+    private double cantidadUmbral;
+    private double cantidadActual;
+    private boolean reserva;
+
+    //Añadimos Constructor parametrizado
+    public Deposito(String contenido, double cantidadMaxima, double cantidadUmbral, double cantidadActual) {
+        this.contenido = contenido;
+        this.cantidadMaxima = cantidadMaxima;
+        this.cantidadUmbral = cantidadUmbral;
+        this.cantidadActual = cantidadActual;
+        estadoReserva();
+
+    }
+
+    private void estadoReserva() {
+        if (this.cantidadActual <= this.cantidadUmbral) {
+            this.reserva = true;
+            System.out.println("La cantidad de " + getContenido() + " ha entrado en reserva, necesitamos recarcar");
+        }
+
+    }
+
+    //Añadimos Getters y Setters
+    public String getContenido() {
+        return contenido;
+    }
+
+    public double getCantidadMaxima() {
+        return cantidadMaxima;
+    }
+
+    public double getCantidadUmbral() {
+        return cantidadUmbral;
+    }
+
+    public double getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public boolean isReserva() {
+        return reserva;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public void setCantidadMaxima(double cantidadMaxima) {
+        this.cantidadMaxima = cantidadMaxima;
+    }
+
+    public void setCantidadUmbral(double cantidadUmbral) {
+        this.cantidadUmbral = cantidadUmbral;
+    }
+
+    public void setCantidadActual(double cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
+
+    public void setReserva(boolean reserva) {
+        this.reserva = reserva;
+    }
+
+    @Override
+    public String toString() {
+        return "Deposito{" + "contenido=" + contenido + ", cantidadMaxima=" + cantidadMaxima + ", cantidadUmbral=" + cantidadUmbral + ", cantidadActual=" + cantidadActual + '}';
+    }
 }
