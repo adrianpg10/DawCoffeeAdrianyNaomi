6bee7b6e956105d76128ec2f3cdb3d74cf155ee1
nao <zarza.naomi@gmail.com>
2020-01-09 14:43:38 +0100

    Añade a la clase Cafetera atributos, constructores, métodos getters, setters y toString

--- src/main/java/dawcoffeeadriannaomi/Cafetera.java
+++ src/main/java/dawcoffeeadriannaomi/Cafetera.java
@@ -10,5 +10,109 @@ package dawcoffeeadriannaomi;
  * @author adrip
  */
 public class Cafetera {
-    
+
+    //Atributos
+    private double saldoCliente; //Saldo actual del cliente
+    private double saldoAcumulado; //Cantidad de dinero en la máquina
+    private int numVentasRealizadas;
+    private Deposito leche;
+    private Deposito cafe;
+    private Deposito chocolate;
+    private Deposito agua;
+    private Deposito azucar;
+
+    //Constructor por defecto
+    public Cafetera() {
+        this.saldoCliente = 0;
+        this.saldoAcumulado = 0;
+        this.numVentasRealizadas = 0;
+        this.leche = new Deposito();
+        this.cafe = cafe;
+        this.chocolate = chocolate;
+        this.agua = agua;
+        this.azucar = azucar;
+    }
+
+    //Constructor parametrizado
+    public Cafetera(double saldoCliente, double saldoAcumulado, int numVentasRealizadas, Deposito leche, Deposito cafe, Deposito chocolate, Deposito agua, Deposito azucar) {
+        this.saldoCliente = saldoCliente;
+        this.saldoAcumulado = saldoAcumulado;
+        this.numVentasRealizadas = numVentasRealizadas;
+        this.leche = leche;
+        this.cafe = cafe;
+        this.chocolate = chocolate;
+        this.agua = agua;
+        this.azucar = azucar;
+    }
+
+    //Métodos getters y setters
+    public double getSaldoCliente() {
+        return saldoCliente;
+    }
+
+    public void setSaldoCliente(double saldoCliente) {
+        this.saldoCliente = saldoCliente;
+    }
+
+    public double getSaldoAcumulado() {
+        return saldoAcumulado;
+    }
+
+    public void setSaldoAcumulado(double saldoAcumulado) {
+        this.saldoAcumulado = saldoAcumulado;
+    }
+
+    public int getNumVentasRealizadas() {
+        return numVentasRealizadas;
+    }
+
+    public void setNumVentasRealizadas(int numVentasRealizadas) {
+        this.numVentasRealizadas = numVentasRealizadas;
+    }
+
+    public Deposito getLeche() {
+        return leche;
+    }
+
+    public void setLeche(Deposito leche) {
+        this.leche = leche;
+    }
+
+    public Deposito getCafe() {
+        return cafe;
+    }
+
+    public void setCafe(Deposito cafe) {
+        this.cafe = cafe;
+    }
+
+    public Deposito getChocolate() {
+        return chocolate;
+    }
+
+    public void setChocolate(Deposito chocolate) {
+        this.chocolate = chocolate;
+    }
+
+    public Deposito getAgua() {
+        return agua;
+    }
+
+    public void setAgua(Deposito agua) {
+        this.agua = agua;
+    }
+
+    public Deposito getAzucar() {
+        return azucar;
+    }
+
+    public void setAzucar(Deposito azucar) {
+        this.azucar = azucar;
+    }
+
+    //Método toString
+    @Override
+    public String toString() {
+        return "Cafetera{" + "saldoCliente=" + saldoCliente + ", saldoAcumulado=" + saldoAcumulado + ", numVentasRealizadas=" + numVentasRealizadas + ", leche=" + leche + ", cafe=" + cafe + ", chocolate=" + chocolate + ", agua=" + agua + ", azucar=" + azucar + '}';
+    }
 }
