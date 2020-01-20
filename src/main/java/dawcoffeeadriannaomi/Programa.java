366ca4271485b4ca2584925e0f3e9fbb42e0c3d3
naomi <zarza.naomi@gmail.com>
2020-01-20 14:31:29 +0100

    Añade correción en la clase Programa

--- src/main/java/dawcoffeeadriannaomi/Programa.java
+++ src/main/java/dawcoffeeadriannaomi/Programa.java
@@ -10,24 +10,27 @@ package dawcoffeeadriannaomi;
  * @author Nao
  */
 public class Programa {
+
     public static void main(String[] args) {
-        
+
         //Creo e instancio el usurio con acceso al panel de administración
         Usuario administrador = new Usuario("adminCafeStoreZ5", "CZr4t89SDF");
+
+        //Creación e instanciación de los componentes de la cafetera
+        Deposito leche = new Deposito("leche", 1000, 1000, false);
+        Deposito cafe = new Deposito("café", 2000, 2000, false);
+        Deposito cafeDescafeinado = new Deposito("café descafeinado", 2000, 2000, false);
+        Deposito chocolate = new Deposito("chocolate", 1000, 1000, false);
+        Deposito agua = new Deposito("agua", 2000, 2000, false);
+        Deposito azucar = new Deposito("azucar", 1000, 1000, false);
+        Deposito sacarina = new Deposito("sacarina", 500, 500, false);
         
-        //Creación e instanciación de la Cafetera y sus componentes
-        Deposito leche = new Deposito("leche", 1000, 300, 1000, false);
-        Deposito cafe = new Deposito("café", 2000, 500, 2000, false);
-        Deposito chocolate = new Deposito("chocolate", 1000, 300, 1000, false);
-        Deposito agua = new Deposito("agua", 2000, 500, 2000, false);
-        Deposito azucar = new Deposito("azucar", 1000, 200, 1000, false);
-        
-        //Cafetera cafetera1 = new Cafetera(double saldoCliente, double saldoAcumulado, int numVentasRealizadas, Deposito leche, Deposito cafe, Deposito chocolate, Deposito agua, Deposito azucar, Usuario admin);
-        Cafetera cafetera = new Cafetera(0, 0, 0, leche, cafe, chocolate, agua, azucar, administrador);
-        
-        //Ejecuto el menú
+        //Creo e instancio la cafetera
+        Cafetera cafetera = new Cafetera(0, 0, leche, cafe, cafeDescafeinado, chocolate, agua, azucar, sacarina, administrador);
+
+        //Ejecuto el menú y le paso la cafetera
         Menu.Menu(cafetera);
-        
+
         //System.out.println(cafetera.toString()); --Comprobación
     }
 }
