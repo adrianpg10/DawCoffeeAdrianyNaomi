c9d33057a685f9145c6ee219b9765857a636f566
nao <zarza.naomi@gmail.com>
2020-01-26 22:15:10 +0100

    Ampliación clase Menu apartado de venta de productos

--- src/main/java/dawcoffeeadriannaomi/Menu.java
+++ src/main/java/dawcoffeeadriannaomi/Menu.java
@@ -38,6 +38,8 @@ public class Menu {
                 case 1:
                     //VENTA DE PRODUCTOS//
                     String opcionesVenta;
+                    String opcionAzucar;
+                    String opcionCantAzucar;
                     //Utilizo un bucle do-while para controlar que el usuario elige una opción disponible
                     do {
                         //Muestro al usuario las opciones disponibles
@@ -47,25 +49,95 @@ public class Menu {
                                 + "A2 - Café solo largo\n"
                                 + "A3 - Café con leche\n"
                                 + "A4 - Café cortado\n"
-                                + "B1 - Chocolate\n"
-                                + "C1 - Leche fría\n"
-                                + "C2 - Leche caliente\n");
+                                + "B1 - Café solo descafeinado\n"
+                                + "B2 - Café solo largo descafeinado\n"
+                                + "B3 - Café descafeinado con leche\n"
+                                + "B4 - Café cortado descafeinado\n"
+                                + "C1 - Chocolate\n"
+                                + "D1 - Leche fría\n"
+                                + "D2 - Leche caliente\n");
                     } while (!(opcionesVenta.equalsIgnoreCase("A1") || opcionesVenta.equalsIgnoreCase("A2") || opcionesVenta.equalsIgnoreCase("A3")
-                            || opcionesVenta.equalsIgnoreCase("A4") || opcionesVenta.equalsIgnoreCase("B1") || opcionesVenta.equalsIgnoreCase("C1")
-                            || opcionesVenta.equalsIgnoreCase("C2")));
-                    
-                    if(opcionesVenta.equalsIgnoreCase("A1")){
-                        cafetera.servirCafeSolo();
-                    }else if(opcionesVenta.equalsIgnoreCase("A2")){
-                        cafetera.servirCafeLargo();
-                    }else if(opcionesVenta.equalsIgnoreCase("A3")){
-                        cafetera.servirCafeLeche();
-                    }else if(opcionesVenta.equalsIgnoreCase("A4")){
-                        cafetera.servirCafeCortado();
-                    }else if(opcionesVenta.equalsIgnoreCase("B1")){
-                        cafetera.servirChocolate();
-                    }else{
-                        cafetera.servirLeche();  
+                            || opcionesVenta.equalsIgnoreCase("A4") || opcionesVenta.equalsIgnoreCase("B1") || opcionesVenta.equalsIgnoreCase("B2")
+                            || opcionesVenta.equalsIgnoreCase("B3") || opcionesVenta.equalsIgnoreCase("B4") || opcionesVenta.equalsIgnoreCase("C1")
+                            || opcionesVenta.equalsIgnoreCase("D1") || opcionesVenta.equalsIgnoreCase("D2")));
+
+                    do {
+                        opcionAzucar = JOptionPane.showInputDialog("[Daw Coffee]\n"
+                                + "¿Desea su bebida con azúcar o sacarina?:\n"
+                                + "1.- Azúcar\n"
+                                + "2.- Sacarina\n");
+                    } while (!(opcionAzucar.equals("1") || opcionAzucar.equals("2")));
+
+                    do {
+                        opcionCantAzucar = JOptionPane.showInputDialog("[Daw Coffee]\n"
+                                + "¿Qué cantidad de edulcorante desea?:\n"
+                                + "1.- Poca\n"
+                                + "2.- Media\n"
+                                + "3.- Mucha\n");
+                    } while (!(opcionAzucar.equalsIgnoreCase("poca") || opcionAzucar.equalsIgnoreCase("media") || opcionAzucar.equalsIgnoreCase("mucha")));
+
+                    switch (opcionAzucar) {
+                        case "1": //HA ELEGIDO AZÚCAR
+                            switch (opcionCantAzucar.toUpperCase()) { //CANTIDAD DE AZÚCAR QUE DESEA
+                                case "POCA":
+                                    cafetera.servirAzucar(1);
+                                    break;
+                                case "MEDIA":
+                                    cafetera.servirAzucar(2);
+                                    break;
+                                case "MUCHA":
+                                    cafetera.servirAzucar(3);
+                                    break;
+                            }
+                            break;
+
+                        case "2": //HA ELEGIDO SACARINA
+                            switch (opcionCantAzucar.toUpperCase()) { //CANTIDAD DE SACARINA QUE DESEA
+                                case "POCA":
+                                    cafetera.servirSacarina(1);
+                                    break;
+                                case "MEDIA":
+                                    cafetera.servirSacarina(2);
+                                    break;
+                                case "MUCHA":
+                                    cafetera.servirSacarina(3);
+                                    break;
+                            }
+                            break;
+                    }
+
+                    switch (opcionesVenta.toUpperCase()) {
+                        case "A1":
+                            cafetera.servirCafeSolo();
+                            break;
+                        case "A2":
+                            cafetera.servirCafeLargo();
+                            break;
+                        case "A3":
+                            cafetera.servirCafeLeche();
+                            break;
+                        case "A4":
+                            cafetera.servirCafeCortado();
+                            break;
+                        case "B1":
+                            cafetera.servirCafeSoloDescafeinado();
+                            break;
+                        case "B2":
+                            cafetera.servirCafeLargoDescafeinado();
+                            break;
+                        case "B3":
+                            cafetera.servirCafeLecheDescafeinado();
+                            break;
+                        case "B4":
+                            cafetera.servirCafeCortadoDescafeinado();
+                            break;
+                        case "C1":
+                            cafetera.servirChocolate();
+                            break;
+                        case "D1":
+                        case "D2":
+                            cafetera.servirLeche();
+                            break;
                     }
 
                     //Muestro los datos al usuario
@@ -90,6 +162,7 @@ public class Menu {
                         //Paso el String "opcion" a int haciendo un parseo
                         opcionElegida = Integer.parseInt(opcionesAdministracion);
                     } while (opcionElegida < 1 || opcionElegida > 4);
+
                     break;
 
                 case 3:
